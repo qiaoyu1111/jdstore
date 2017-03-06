863671bbf78ef00db644a75979dba8c092ce9bd7
qiaoyu <qiaoyu1111358@gmail.com>
2017-03-06 23:15:58 +0800

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,31 @@ u.password_confirmation = "123456"   # 最少要六码
 
 u.is_admin = true
 u.save
+
+Product.create!(title: "花飞扬",
+                 description: "一点一滴 艳丽沉浸",
+                 price: 2800,
+                 quantity: 10,
+                 image: open("https://ww4.sinaimg.cn/large/006tNbRwgy1fddi6tqkg9j31kw11xhdv.jpg")
+                 )
+
+Product.create!(title: "春日暖",
+                 description: "面朝大海 春暖花开",
+                 price: 2800,
+                 quantity: 10,
+                 image: open("https://ww1.sinaimg.cn/large/006tNbRwgy1fddi5tvugjj31kw11x4qr.jpg")
+                 )
+
+Product.create!(title: "女人香",
+                 description: "发丝唇边耳畔 都是你",
+                 price: 2800,
+                 quantity: 10,
+                 image: open("https://ww2.sinaimg.cn/large/006tNbRwgy1fddi4o2vhkj31kw11xhdu.jpg")
+                 )
+
+Product.create!(title: "灿烂心",
+                 description: "回眸一笑 花飞扬",
+                 price: 2800,
+                 quantity: 10,
+                 image: open("https://ww1.sinaimg.cn/large/006tNbRwgy1fddi3qmltij31kw11xe82.jpg")
+                 )
