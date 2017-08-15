c43bd81078ee77645586759ee0c80c38df1d7e1b
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-15 17:11:19 +0800

    add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,21 +7,31 @@ class ApplicationController < ActionController::Base
     end
   end
 
-   helper_method :current_cart
+  helper_method :current_cart
 
-   def current_cart
-     @current_cart ||= find_cart
+  def current_cart
+   @current_cart ||= find_cart
+  end
+
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
+
+  private
+
+  def find_cart
+   cart = Cart.find_by(id: session[:cart_id])
+   if cart.blank?
+     cart = Cart.create
    end
-
-   private
-
-   def find_cart
-     cart = Cart.find_by(id: session[:cart_id])
-     if cart.blank?
-       cart = Cart.create
-     end
-     session[:cart_id] = cart.id
-     return cart
-   end
+   session[:cart_id] = cart.id
+   return cart
+  end
 
 end
